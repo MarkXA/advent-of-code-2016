0af265f5ab50532386340d432eae62d8f59eefd6
Mark Allan <mark@ranyart.com>
2016-12-03 19:21:00 +0000

    Day 3

--- AdventOfCode2016/Program.fs
+++ AdventOfCode2016/Program.fs
@@ -6,4 +6,6 @@ let main argv =
     printfn "Day 1 part 2: %A" Day1.part2
     printfn "Day 2 part 1: %A" Day2.part1
     printfn "Day 2 part 2: %A" Day2.part2
+    printfn "Day 3 part 1: %A" Day3.part1
+    printfn "Day 3 part 2: %A" Day3.part2
     0
